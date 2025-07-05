ec528e31758c032830a1b031f5bfc2ce0b49347d
haimin13 <gkals862@naver.com>
2025-07-05 20:31:59 +0900

    friends tab column 구현 진행중1

--- app/src/main/java/com/example/myapplication1/ui/screens/FriendsTab.kt
+++ app/src/main/java/com/example/myapplication1/ui/screens/FriendsTab.kt
@@ -1,18 +1,65 @@
 package com.example.myapplication1.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myapplication1.R
+
+@Composable
+fun CloseFriendsRowEntry(modifier: Modifier = Modifier) {
+
+}
 
 @Composable
 fun FriendsTabMain(modifier: Modifier = Modifier) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        modifier = Modifier.fillMaxSize()
     ) {
-        Text("Friends Tab Screen")
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp, end = 10.dp),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.Notifications,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(40.dp)
+            )
+        }
+        Column(
+            modifier = Modifier.fillMaxWidth()
+                .padding(horizontal = 15.dp)
+        ) {
+            Text(
+                text = "close friends",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.ExtraBold
+            )
+            LazyRow(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+
+            }
+        }
     }
 }
