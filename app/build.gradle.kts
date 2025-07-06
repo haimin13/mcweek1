e36ec26bae0c30277b69e853be3b90642f60d950
haimin13 <gkals862@naver.com>
2025-07-06 18:44:12 +0900

    fix: compileOptions에서 desugaring 활성화
    - compileOptions 블록에 isCoreLibraryDesugaringEnabled = true 추가
    - java.time API의 API 레벨 26 요구사항 에러 해결
    - minSdk 23에서 LocalDateTime.now() 사용 가능하도록 수정
    
    feat: friends/notification page 구현
    
    -페이지 및 엔트리 구현 완료
    -drawable 추가함

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,7 @@ android {
         }
     }
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -58,4 +59,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
